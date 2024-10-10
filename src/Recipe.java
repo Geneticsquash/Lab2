748f738072e11d0c3deb99e462dade476deea8d7
Geneticsquash <calle-daag@hotmail.com>
2024-10-10 16:45:27 +0200

    commit message

---
+++ src/Recipe.java
@@ -0,0 +1,54 @@
+public class Recipe extends Categories {
+    // https://www.youtube.com/watch?v=K1iu1kXkVoA
+    // https://www.youtube.com/watch?v=k_SJOnVa_G0
+    private String recipeName;
+    private int recipeNumber;
+
+    public Recipe(int recipeNumber, String recipeName) {
+        this.recipeNumber = recipeNumber;
+        this.recipeName = recipeName;
+    }
+
+    public Recipe() {
+
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public int getRecipeNumber() {
+        return recipeNumber;
+    }
+
+    public void setRecipeNumber(int recipeNumber, Recipe obj) {
+        Recipe obj1 = obj;
+        this.recipeNumber = recipeNumber;
+    }
+    public class Demo
+    {
+
+        public static void main(String[] args)
+        {  Recipe obj= new Recipe();
+            obj.setRecipeNumber(1);
+            obj.setRecipeName("Spicy Swedish Meatballs");
+
+            System.out.println(obj.getRecipeName() + " " + obj.getRecipeNumber());
+
+        }
+    }
+}
+
+   
+
+
+
+
+
+
+
+
---
+++ src/RecipeDemoTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+Create unit tests using JUnit to verify the functionality of classes and methods.
+At least three tests should be created to ensure recipes can be added, removed, and displayed correctly.
+The tests should be placed in a separate folder named "test" within the project.
+ */
+
+class RecipeDemoTest {
+
+    @org.junit.jupiter.api.Test
+    void getRecipeName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRecipeName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRecipeNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRecipeNumber() {
+    }
+}
