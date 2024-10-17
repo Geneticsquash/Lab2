af52ee730687eb042a8b4c5bbedb14302cb39e5e
Geneticsquash <calle-daag@hotmail.com>
2024-10-17 13:22:14 +0200

    It still doesnt work AAAAAAAAAAAAAAAAAAAArgh

---
+++ src/test/addDefaultRecipeTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+//TODO The point of having tests is that it helps you
+// spot errors during the multiple years of code maintenance after writing it initially.
+class addDefaultRecipeTest {
+
+
+    public addDefaultRecipeTest() {
+        int addDefaultRecipeTest;
+    }
+}
